e33288a381bc326a8921bb0d7423bbd9a28b7e8a
RedpathsRepos <christim@science.oregonstate.edu>
2013-09-30 17:58:31 -0400

    Deletion of old modules

--- source/IsolationMatrices.R
+++
@@ -1,8 +0,0 @@
-IsolationMatrices <- function(migration.matrix, n.isolates) {
-  ## change migration matrix to allow for isolation in n.isolates sites
-  r.isolate <- 1:n.isolates
-  migration.matrix[r.isolate, ] <- 0
-  return(migration.matrix)
-}
-
-
